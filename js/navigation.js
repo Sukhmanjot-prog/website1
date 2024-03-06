eba74cc19c6c4b0d3c27b812eabee4bfeb950d1c
Sukhmanjot-prog <142855362+Sukhmanjot-prog@users.noreply.github.com>
2024-03-06 17:06:13 +0000

    Wed, Mar 6, 2024, 9:06 AM -08:00

--- js/navigation.js
+++ js/navigation.js
@@ -3,5 +3,10 @@ nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    console.log("hello")
+    console.log(window.scrollY)
+    if (window.scroll > 223) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
 }
